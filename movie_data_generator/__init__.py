a22dc0cccde90cd271ea42ae914d8ba2d3071e73
Tobias Knuth <tknuth@users.noreply.github.com>
2024-03-01 13:48:39 +0100

    deleted files

--- movie_data_generator/__init__.py
+++
@@ -1,6 +0,0 @@
-from .genre import *
-from .scoring import *
-from .movie import *
-from .profile import *
-from .synthesis import *
-from .user import *
--- tests/test_user.py
+++
@@ -1,6 +0,0 @@
-# from movie_data_generator.user import User
-
-
-# def test_instantiate_user():
-#     profile = {Genre.WESTERN: 0.8, Genre.ACTION: 0.8, Genre.ADVENTURE: 0.4}
-#     User(coverage=0.8, profile=profile)
