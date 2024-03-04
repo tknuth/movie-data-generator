12c464b8e0eb627c549fa5b60cd0e497e6bcdaaf
Tobias Knuth <tknuth@users.noreply.github.com>
2024-03-04 20:18:44 +0100

    use tuple instead of copied list

--- movie_data_generator/profile.py
+++ movie_data_generator/profile.py
@@ -1,19 +1,16 @@
 from .genre import *
-import copy
 
 Profile = dict[Genre, float]
 
 
 def load_profiles():
-    return copy.copy(
-        [
-            {Genre.ADVENTURE: 6, Genre.ACTION: 4},
-            {Genre.THRILLER: 5, Genre.DRAMA: 5},  # crime, mystery
-            {Genre.FANTASY: 5, Genre.ADVENTURE: 3, Genre.ANIMATION: 3},
-            {Genre.DOCUMENTARY: 7, Genre.HISTORY: 3},  # realist
-            {Genre.ROMANCE: 6, Genre.DRAMA: 2, Genre.COMEDY: 2},  # romcom/dramedy
-            {Genre.WESTERN: 6, Genre.ACTION: 2, Genre.ADVENTURE: 2},
-        ]
+    return (
+        {Genre.ADVENTURE: 6, Genre.ACTION: 4},
+        {Genre.THRILLER: 5, Genre.DRAMA: 5},  # crime, mystery
+        {Genre.FANTASY: 5, Genre.ADVENTURE: 3, Genre.ANIMATION: 3},
+        {Genre.DOCUMENTARY: 7, Genre.HISTORY: 3},  # realist
+        {Genre.ROMANCE: 6, Genre.DRAMA: 2, Genre.COMEDY: 2},  # romcom/dramedy
+        {Genre.WESTERN: 6, Genre.ACTION: 2, Genre.ADVENTURE: 2},
     )
 
 
